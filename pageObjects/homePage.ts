e6c960197a8925beb12b98e49122855d61844e84
Siddhant Awasthi <siddhantawasthi009@gmail.com>
2026-03-04 23:53:29 +0530

    added new files

--- pageObjects/homePage.ts
+++ pageObjects/homePage.ts
@@ -1,6 +1,7 @@
 import { Locator, Page } from "@playwright/test";
 
 export class HomePage {
+    
     readonly page: Page;
     readonly btnClosePopup: Locator;
     readonly logoHomePage: Locator;
